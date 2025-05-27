ea1633008c9c76315a892e04293f4a5d4236fc8d
明轩 朱 <836145715@qq.com>
2025-05-27 08:10:11 +0800

    初始化项目

---
+++ quick-server/src/main/java/com/zmx/quickserver/QuickServerApplication.java
@@ -0,0 +1,13 @@
+package com.zmx.quickserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickServerApplication.class, args);
+    }
+
+}
---
+++ quick-server/src/test/java/com/zmx/quickserver/QuickServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmx.quickserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
