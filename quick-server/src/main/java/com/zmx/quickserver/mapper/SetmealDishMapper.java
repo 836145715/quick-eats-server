bb3ca8e2779e8e9ef1920849dfb3de1c5890cab8
明轩 朱 <836145715@qq.com>
2025-06-05 08:17:34 +0800

    feat(mapper): 添加菜品和套餐菜品关系 mapper 接口
    - 在 DishMapper 中添加 countByCategoryId 方法，用于根据分类 ID 查询菜品数量
    - 在 SetmealDishMapper 中添加 countByCategoryId 方法，用于根据分类 ID 查询套餐数量

--- quick-server/src/main/java/com/zmx/quickserver/mapper/SetmealDishMapper.java
+++ quick-server/src/main/java/com/zmx/quickserver/mapper/SetmealDishMapper.java
@@ -3,10 +3,21 @@ package com.zmx.quickserver.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zmx.quickpojo.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * 套餐菜品关系Mapper接口
  */
 @Mapper
 public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
+
+    /**
+     * 根据分类id查询菜品数量
+     * @param categoryId
+     * @return
+     */
+    @Select("select count(id) from setmeal where category_id =  #{categoryId}")
+    Integer countByCategoryId(Long categoryId);
+
+
 }
